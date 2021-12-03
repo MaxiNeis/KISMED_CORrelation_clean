74a2a231f30b3cd1f87119b51ab36d28786b9d55
MaxiNeis <maximilian.neis@gmx.de>
2021-12-03 23:55:57 +0100

    added predict.py

--- predict.py
+++ predict.py
@@ -8,6 +8,8 @@ Skript testet das vortrainierte Modell
 """
 
 import csv
+
+import keras.models
 import scipy.io as sio
 import matplotlib.pyplot as plt
 import numpy as np
@@ -41,20 +43,75 @@ def predict_labels(ecg_leads : List[np.ndarray], fs : float, ecg_names : List[st
 
 #------------------------------------------------------------------------------
 # Euer Code ab hier  
-    with open(model_name, 'rb') as f:  
-        th_opt = np.load(f)         # Lade simples Model (1 Parameter)
-
-    detectors = Detectors(fs)        # Initialisierung des QRS-Detektors
+    #with open(model_name, 'rb') as f:
+    model = keras.models.load_model(model_name)        # Lade simples Model (1 Parameter)
+            # Initialisierung des QRS-Detektors
 
     predictions = list()
-    
+    minimum = 2714
+
+    def mediansmooth(window, sliding, array):
+        r = int(np.floor((len(array) - window) / sliding) + 1)
+        new_array = np.zeros(r)
+        for i in range(0, r):
+            subarray = np.zeros(window)
+            for j in range(0, window):
+                subarray[j] = array[i * sliding + j]
+            median = np.median(subarray)
+            new_array[i] = median
+        return new_array
+
+    def scalearray(array):
+        mini = min(array)
+        maxi = max(array)
+        diff = maxi - mini
+        for i in range(0, len(array)):
+            array[i] = (array[i] - mini) / diff
+        return array
+
     for idx,ecg_lead in enumerate(ecg_leads):
-        r_peaks = detectors.hamilton_detector(ecg_lead)     # Detektion der QRS-Komplexe
-        sdnn = np.std(np.diff(r_peaks)/fs*1000) 
-        if sdnn < th_opt:
-            predictions.append((ecg_names[idx], 'N'))
+        anzahl = len(ecg_lead)
+        faktorceil = int(np.ceil(anzahl / minimum))
+        if faktorceil > 1:
+            biggersize = faktorceil * minimum - anzahl
+            overlapping = np.ceil(biggersize / (faktorceil - 1))
+            ecg_data = np.zeros((faktorceil,minimum))
+            for j in range(0, faktorceil):
+                start = int(j * (minimum - overlapping))
+                end = int(start + minimum)
+                ecg_data[j] = ecg_lead[start: end]
+            new_array = mediansmooth(5,4,ecg_data[0])
+            ecg_smoothed = np.zeros((len(ecg_data), len(new_array)))
+            predictarray = np.zeros((len(ecg_data), 2))
+            boolvalue = 0
+            for l in range(0, len(ecg_data)):
+                ecg_smoothed[l] = mediansmooth(5,4,ecg_data[l])
+                ecg_smoothed[l] = scalearray(ecg_smoothed[l])
+                #ecg_smoothed[l] = ecg_smoothed[l].reshape((678, 1))
+                #predictarray[l] = model.predict(ecg_smoothed[l])[0]
+                #if predictarray[l] < 0.5 :
+                    #boolvalue = 1
+            predictarray = model.predict(ecg_smoothed.reshape((ecg_smoothed.shape[0],ecg_smoothed.shape[1],1)))
+            for d in range(0, len(ecg_data)):
+                if predictarray[d][0] <= 0.5:
+                    boolvalue = 1
+            if  boolvalue == 1 :
+                predictions.append((ecg_names[idx], 'A'))
+            else:
+                predictions.append((ecg_names[idx], 'N'))
         else:
+            # ecg_data = np.zeros(minimum)
+            # for k in range(0, anzahl):
+            #     ecg_data[k] = ecg_lead[k]
+            # ecg_smoothed = mediansmooth(5,4,ecg_data)
+            # ecg_smoothed = scalearray(ecg_smoothed)
+            # predict = model.predict(ecg_smoothed)[0]
+            # if predict < 0.5 :
+            #     predictions.append((ecg_names[idx], 'A'))
+            # else:
+            #     predictions.append((ecg_names[idx], 'N'))
             predictions.append((ecg_names[idx], 'A'))
+
         if ((idx+1) % 100)==0:
             print(str(idx+1) + "\t Dateien wurden verarbeitet.")
             
